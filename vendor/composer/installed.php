506d461376370c7789b5fd26956522e9404311ba
Oberdan Brito <oberdan@craos.net>
2020-06-25 09:15:33 -0300

    Atualização das configurações composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,26 @@
+<?php return array(
+    'root' =>
+        array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'aliases' =>
+                array(
+                    0 => '2.x-dev',
+                ),
+            'reference' => '3eef219b1a3aa774a32fd1f73b2b714874b85b2a',
+            'name' => 'craos/pgdump',
+        ),
+    'versions' =>
+        array(
+            'craos/pgdump' =>
+                array(
+                    'pretty_version' => 'dev-master',
+                    'version' => 'dev-master',
+                    'aliases' =>
+                        array(
+                            0 => '2.x-dev',
+                        ),
+                    'reference' => '3eef219b1a3aa774a32fd1f73b2b714874b85b2a',
+                ),
+        ),
+);
